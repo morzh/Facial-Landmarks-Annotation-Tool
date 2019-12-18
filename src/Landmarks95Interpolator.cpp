1ec55d70305d2bc82d3c1e5f64f267830a43421f
morzhakovanton <morzh12071980>
2019-12-19 02:21:03 +0400

    implementing spline interpolation #2

--- src/Landmarks95Interpolator.cpp
+++ src/Landmarks95Interpolator.cpp
@@ -6,7 +6,7 @@
 #include "spline_library/spline.h"
 #include "spline_library/splines/uniform_cr_spline.h"
 
-void Landmarks95Interpolator::interpolate(std::vector<ft::FaceFeature*> vFeauters) {
+void Landmarks95Interpolator::interpolate(const std::vector<ft::FaceFeature*>& vFeauters) {
 
     interpolate_ovLow(vFeauters);
     interpolate_ovUpper(vFeauters);
@@ -14,46 +14,75 @@ void Landmarks95Interpolator::interpolate(std::vector<ft::FaceFeature*> vFeauter
     interpolate_lBrow(vFeauters);
     interpolate_rEye(vFeauters);
     interpolate_lEye(vFeauters);
+    interpolate_noseRidge(vFeauters);
+    interpolate_noseShape(vFeauters);
 
 }
 
-std::vector<QVector2D> Landmarks95Interpolator::placePointsEvenly(const std::vector<QVector2D> &pts) {
+void Landmarks95Interpolator::placePointsEvenly(std::vector<QVector2D> &pts) {
 
-    UniformCRSpline<QVector2D> mySpline(pts);
-    QVector2D interpolatedPosition = mySpline.getPosition(0.5f);
+    UniformCRSpline<QVector2D>  spline(pts);
+    int num_points = pts.size();
 
+    for (int idx=1; idx<num_points-1; ++idx)
 
+        pts[idx] = getPositionAtLength(spline, idx);
 }
 
 
-void Landmarks95Interpolator::interpolate_ovLow(std::vector<ft::FaceFeature *> vFeauters) {
+void Landmarks95Interpolator::interpolate_ovLow(const std::vector<ft::FaceFeature *>& vFeatures) {
 
-    std::vector<QVector2D> ovalLow(16);
+    std::vector<QVector2D> ovalLowRight(8);
+    std::vector<QVector2D> ovalLowLeft(8);
 
-    for (int idx=0; idx<17; ++idx){
-        ovalLow[idx] = vFeauters[idx]->toVector();
+    for (int idx=0; idx<8; ++idx)
+        ovalLowRight[idx] = vFeatures[idx]->toVector();
+    for (int idx=8; idx<17; ++idx)
+        ovalLowLeft[idx-8] = vFeatures[idx]->toVector();
+
+    placePointsEvenly(ovalLowRight);
+    placePointsEvenly(ovalLowLeft);
+
+    for (int idx=0; idx<7; ++idx) {
+        vFeatures[idx]->setX(ovalLowRight[idx].x());
+        vFeatures[idx]->setY(ovalLowRight[idx].y());
+    }
+    for (int idx=9; idx<17; ++idx) {
+        vFeatures[idx]->setX(ovalLowLeft[idx-8].x());
+        vFeatures[idx]->setY(ovalLowLeft[idx-8].y());
     }
 
-    ovalLow = placePointsEvenly(ovalLow);
+}
+
+void Landmarks95Interpolator::interpolate_ovUpper(const std::vector<ft::FaceFeature *>& vFeatures) {
 
 }
 
-void Landmarks95Interpolator::interpolate_ovUpper(std::vector<ft::FaceFeature *> vFeauters) {
+void Landmarks95Interpolator::interpolate_rBrow(const std::vector<ft::FaceFeature *>& vFeatures) {
 
 }
 
-void Landmarks95Interpolator::interpolate_rBrow(std::vector<ft::FaceFeature *> vFeauters) {
+void Landmarks95Interpolator::interpolate_lBrow(const std::vector<ft::FaceFeature *>& vFeatures) {
 
 }
 
-void Landmarks95Interpolator::interpolate_lBrow(std::vector<ft::FaceFeature *> vFeauters) {
+void Landmarks95Interpolator::interpolate_rEye(const std::vector<ft::FaceFeature *>& vFeatures) {
 
 }
 
-void Landmarks95Interpolator::interpolate_rEye(std::vector<ft::FaceFeature *> vFeatures) {
+void Landmarks95Interpolator::interpolate_lEye(const std::vector<ft::FaceFeature *>& vFeatures) {
 
 }
 
-void Landmarks95Interpolator::interpolate_lEye(std::vector<ft::FaceFeature *> vFeatures) {
+void Landmarks95Interpolator::interpolate_noseRidge(const std::vector<ft::FaceFeature *> &vector) {
 
+}
+
+void Landmarks95Interpolator::interpolate_noseShape(const std::vector<ft::FaceFeature *> &vector) {
+
+}
+
+QVector2D Landmarks95Interpolator::getPositionAtLength(UniformCRSpline<QVector2D> spline, double length_parm) {
+
+    return QVector2D();
 }
--- src/Landmarks95Interpolator.h
+++ src/Landmarks95Interpolator.h
@@ -6,25 +6,32 @@
 #define FLAT_LANDMARKS95INTERPOLATOR_H
 
 #include <QtGui/QVector2D>
+#include <src/spline_library/splines/uniform_cr_spline.h>
 #include "facefeature.h"
 
 
 
 class Landmarks95Interpolator {
 public:
-    void interpolate(std::vector<ft::FaceFeature*>);
+    void    interpolate(const std::vector<ft::FaceFeature*>&);
 
 private:
-    void                    interpolate_ovLow(std::vector<ft::FaceFeature*> vFeauters);
-    void                    interpolate_ovUpper(std::vector<ft::FaceFeature*> vFeauters);
+    void    interpolate_ovLow(const std::vector<ft::FaceFeature*>& vFeatures);
+    void    interpolate_ovUpper(const std::vector<ft::FaceFeature*>& vFeatures);
 
-    void                    interpolate_rBrow(std::vector<ft::FaceFeature*> vFeauters);
-    void                    interpolate_lBrow(std::vector<ft::FaceFeature*> vFeauters);
+    void    interpolate_rBrow(const std::vector<ft::FaceFeature*>& vFeatures);
+    void    interpolate_lBrow(const std::vector<ft::FaceFeature*>& vFeatures);
 
-    void                    interpolate_rEye(std::vector<ft::FaceFeature *> vFeatures);
-    void                    interpolate_lEye(std::vector<ft::FaceFeature *> vFeatures );
+    void    interpolate_rEye(const std::vector<ft::FaceFeature *>& vFeatures);
+    void    interpolate_lEye(const std::vector<ft::FaceFeature *>& vFeatures );
 
-    std::vector<QVector2D>  placePointsEvenly(const std::vector<QVector2D> &pts);
+    void    placePointsEvenly(std::vector<QVector2D> &pts);
+
+    void interpolate_noseRidge(const std::vector<ft::FaceFeature *> &vector);
+
+    void interpolate_noseShape(const std::vector<ft::FaceFeature *> &vector);
+
+    QVector2D getPositionAtLength(UniformCRSpline<QVector2D> spline, double length_parm);
 };
 
 
